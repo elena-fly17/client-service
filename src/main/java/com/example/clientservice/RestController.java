11c1e1015a35c7eabd467118d7b60bfb271d8a7b
elena-fly17 <elena17mr@gmail.com>
2023-05-30 15:22:17 +0300

    Подключила ELK (Elasticsearch + Logstash + Kibana)

--- src/main/java/com/example/clientservice/RestController.java
+++ src/main/java/com/example/clientservice/RestController.java
@@ -1,6 +1,9 @@
 package com.example.clientservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +12,11 @@ import java.util.List;
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/client")
 public class RestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RestController.class);
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     private FeignUtil feignUtil;
@@ -21,5 +29,13 @@ public class RestController {
     @GetMapping("/test")
     public String getTestStringLoadBalancer() {
         return "Test string from client-service";
+    }
+
+    @RequestMapping("/test_logging")
+    public String home() {
+        String home = "Client-Service running at port: "
+                + environment.getProperty("local.server.port");
+        LOG.info(home);
+        return home;
     }
 }
